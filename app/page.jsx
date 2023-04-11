a4fba300e09e5051526c348e68146ac972d56d93
Rachel Tucker <rach.mello@hotmail.com>
2023-04-11 13:12:50 -0500

    Created function to fetch data from OpenAI API

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Prompt from './prompt/route';
+import './page.module.css';
+
+export default function MixMatchPage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div>
+        <Prompt/>
+      </div> 
+  </div>
+  )
+}
+
